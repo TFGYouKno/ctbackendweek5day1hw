d2f684d5b94b79665b9838e6b9f2b6b18d4b08a2
TFGYouKno <brockhoffharrison@comcast.net>
2024-07-16 16:57:17 -0400

    initial commit

---
+++ readme.py
@@ -0,0 +1,24 @@
+#1. Designing and Implementing a Relational Database for a Bookstore
+
+
+#Objective: The aim of this assignment is to apply the concepts of relational database design, normalization, and entity-relationship modeling to create an efficient and structured database for a bookstore management system, "BookHaven."
+
+
+
+#Problem Statement: "BookHaven" is facing challenges in managing its extensive collection of books, author information, customer data, and transaction records. The current system has issues with data redundancy, inconsistency, and inefficient data retrieval. Your task is to design a relational database that addresses these problems while ensuring data integrity and ease of access.
+
+#Design a normalized database ERD for "BookHaven". Which should be able to handle the following.
+#A Customer can make many Transactions
+#A Transaction can include many Books 
+#A Book can be sold many times
+#A Book is only written by one Author
+#An Author can write many books
+
+
+#Task 1: Entity-Relationship Diagram (ERD) Creation
+
+#Develop an ERD to visually represent the relevant data and datatypes that would be stored in the the tables Customer, Transactions, Books, Author .
+#The cardinality should accurately represent the relationships such as the link between Books and Authors,  Customers and Transactions. Transactions and Books (hint: this is a many-to-many relationship)
+
+
+#Expected Outcome: A clear and detailed ERD that accurately reflects the relationships within the "BookHaven" database.
